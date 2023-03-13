fc91e3c9f5241d36a58fa229d16ca39e7410e75e
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-03-13 18:22:48 +0100

    Band generator create

---
+++ band-generator.py
@@ -0,0 +1,8 @@
+#Greeting
+print("Welcome to Band name Generator.")
+#Name of the city an user grew up in
+city_name = input("What is name of the city you grew up in?\n")
+#Users pet name
+pet_name = input("What was your pets name?\n")
+#Combining both names to create a band name
+print("Your band name could be " + city_name +" "+ pet_name)
