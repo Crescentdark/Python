028fb8429384866e3b7f6bf5f21c04aec7891451
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-03-15 11:32:59 +0100

    Adding excersises perfomed with 100 days of code

---
+++ Excerssises/BMI - calculator excersie.py
@@ -0,0 +1,7 @@
+#Excersises done with 100 days of code
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+#My code below this
+bmi = int(weight) / float(height) ** 2
+print(int(bmi))
