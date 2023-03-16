ab63505466de98bbb61e03905fd1d005f24fdc73
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-03-16 22:17:42 +0100

    Add the core choices

--- Spells_and_Lairs.py
+++ Spells_and_Lairs.py
@@ -57,3 +57,20 @@ print('''
 ''')
 print("WANDERER! Screams a mad lookin fellar 'Bael Ur Moras Keep WILL EAT YOU ALIVE HAHAHAHAHAHAH'.")
 print("Your bounty is to find the ancient artifact in this wretched place, for the noble has paid.")
+
+door = input("Do you enter trough the backdoor or the front gate?")
+action1 = input("You entered trough the backdoor,where do you go next up the stairs or trough the storage?")
+action2 = input("You enter a library of sorts,one of these books could be a hidden room which do you choose?")
+
+if door == "front gate":
+    print("A living armor has pumelled you.GAME OVER")
+    if action1 == "storage":
+        print("It was a storage of Highly concentrated Mountain Dew, your bones crumble and your skin melts. GAME OVER")
+        if action2 == "White" or "Gold" or "Black":
+            print("Unlucky a disentigration spell was cast.GAME OVER")
+if door == "backdoor":
+    if action1 == "stairs":
+        if action2 == "Black":
+            print("YOu have found the ancient artifact now leg it.VICTORY")
+
+
