49345065d27802fe006e43ccf97d98fa34f56221
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-07-16 22:35:08 +0200

    Project Finished

--- higher_lower_game/higher_lower_game.py
+++ higher_lower_game/higher_lower_game.py
@@ -2,6 +2,7 @@ from art import logo
 from art import vs
 from game_data import data
 import random
+
 #Format said data
 def format_data(account):
     account_name = account["name"]
@@ -20,36 +21,44 @@ def check_answer(guess, a_followers, b_followers):
 #disply art
 print(logo)
 score = 0
-#Random account generation
-account_a = random.choice(data)
+game_should_continue = True
 account_b = random.choice(data)
-if account_a == account_b:
+
+while game_should_continue:
+    #Random account generation
+    account_a = account_b
     account_b = random.choice(data)
+    
+    while account_a == account_b:
+        account_b = random.choice(data)
 
 
-print(f"Compare A: {format_data(account_a)}")
-print(vs)
-print(f"Compare B: {format_data(account_b)}")
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}")
 
-#ask user for a guess
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    #ask user for a guess
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
 
-#check if user correct
-## Get follower count of each account
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
+    #check if user correct
+    ## Get follower count of each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
 
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-## Use IF statement to check if user is correct
+    ## Clear screen
+        #clear fucntion here
+        #print
 
-#Give user feedback on their guess.
-if is_correct:
-    score += 1
-    print(f"You are right! Currect score: {scorfe}")
-else:
-    print(f"Sorry, that's wrong.Final score: {score}")
+    #Give user feedback on their guess.
+    if is_correct:
+        score += 1
+        print(f"You are right! Currect score: {scorfe}")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong.Final score: {score}")
 
 
 #Score storage
