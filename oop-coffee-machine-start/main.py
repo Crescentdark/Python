1921c5ba22eaf2a0a8e9a5d40418315673c51e2d
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-07-20 20:54:18 +0200

    Add some more objects

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,13 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on =
+
+money_machine.report
+coffee_maker.report()
+
+
+while
