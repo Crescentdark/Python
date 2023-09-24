e7fe92169d287d081d29738b29ff71a44c9e1ae0
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-09-24 16:35:32 +0200

    Add scoreboard

---
+++ Snake-game/scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+ALIGMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("Purple")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}",align=ALIGMENT, font=FONT)
+
+    
+    def increase_score(self):
+        self.score +=1
+        self.clear()
+        self.write(f"Score: {self.score}",align="center", font=("Arial", 8, "normal"))
