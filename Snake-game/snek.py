ace076b8c238f0e71ebe8870a9200ecbd08262b7
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-09-24 16:35:45 +0200

    Add Main game body

---
+++ Snake-game/snek.py
@@ -0,0 +1,34 @@
+
+from turtle import Screen
+import time
+from snekclass import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Hyper snake")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+
+screen.exitonclick()
