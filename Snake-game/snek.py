990179df6703df5f40da8938a41f06c6dda71d48
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-09-24 16:43:50 +0200

    Add Game over to the main game

--- Snake-game/snek.py
+++ Snake-game/snek.py
@@ -27,8 +27,15 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+#Eating food
+
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+#Detect collision
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 
 screen.exitonclick()
