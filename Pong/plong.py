70c2afdad36c537a96c4085bb28ea70c82d4b4b4
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-05 20:13:46 +0200

    Add Right paddle var

--- Pong/plong.py
+++ Pong/plong.py
@@ -6,21 +6,7 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = paddle((350,0))
 
 screen.listen()
 screen.onkey(go_up, "up")
