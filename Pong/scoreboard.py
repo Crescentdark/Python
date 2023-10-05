fc39b38462af7547b5118e5af64efdf14d05e4dd
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-05 21:21:53 +0200

    Add ball speed and score increasing

--- Pong/scoreboard.py
+++ Pong/scoreboard.py
@@ -14,7 +14,7 @@ class Scoreboard(Turtle):
 
 
     def update_scoreboard(self):
-        self.clear()
+        self.clear()        
         self.goto(-100,200)
         self.write(self.l_score, align="center", font=("Courier", 88, "normal"))
         self.goto(100,200)
