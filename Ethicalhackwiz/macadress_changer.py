0981ea4d86bacca7d3472378b8b8978790c0b0f0
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-07 11:29:39 +0200

    Finish changer logic

--- Ethicalhackwiz/macadress_changer.py
+++ Ethicalhackwiz/macadress_changer.py
@@ -6,3 +6,5 @@ new_mac = input("new MAC >")
 print(f"[+] Changing Mac address for {interface} to {new_mac} ")
 
 subprocess.call(f"ifconfig {interface} down", shell=True)
+subprocess.call(f"ifconfig {interface} hw ether" + new_mac, shell=True)
+subprocess.call(f"ifconfig {interface} up", shell=True)
