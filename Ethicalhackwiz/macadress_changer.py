d4fa486c1bb5cf8415be39aff10ffacad09120e6
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-07 12:00:00 +0200

    Add error message statements and logic

--- Ethicalhackwiz/macadress_changer.py
+++ Ethicalhackwiz/macadress_changer.py
@@ -6,7 +6,13 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i","--interface", dest="interface", help="Interface that changes Mac adress")
     parser.add_option("-m","--mac", dest="new_mac", help="New Mac adress")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, or use --help")
+    elif not options.new_mac:
+        parser.error("[-] Please specify an interface, or use --help")
+    return options
+
 
 def change_mac(interface, new_mac):
     print(f"[+] Changing Mac address for {interface} to {new_mac} ")
@@ -16,6 +22,6 @@ def change_mac(interface, new_mac):
     subprocess.call(f"ifconfig {interface} up")
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
 
