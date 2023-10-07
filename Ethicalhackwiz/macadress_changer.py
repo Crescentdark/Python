72b93abccd6284368073a951c0d6497c3d92cb4f
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-07 11:31:12 +0200

    Restructer to be less hijacable

--- Ethicalhackwiz/macadress_changer.py
+++ Ethicalhackwiz/macadress_changer.py
@@ -5,6 +5,6 @@ new_mac = input("new MAC >")
 
 print(f"[+] Changing Mac address for {interface} to {new_mac} ")
 
-subprocess.call(f"ifconfig {interface} down", shell=True)
-subprocess.call(f"ifconfig {interface} hw ether" + new_mac, shell=True)
-subprocess.call(f"ifconfig {interface} up", shell=True)
+subprocess.call(f"ifconfig {interface} down")
+subprocess.call(f"ifconfig {interface} hw ether" + new_mac,)
+subprocess.call(f"ifconfig {interface} up")
