7867b7278303cf70bb27e1c44bd77d3246014ad7
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-09 23:03:55 +0200

    Add scan function groundwork

--- Ethicalhackwiz/scanner.py
+++ Ethicalhackwiz/scanner.py
@@ -5,3 +5,7 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t","--target", dest="target",help="target ip")
     options = parser.parse_args()
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
