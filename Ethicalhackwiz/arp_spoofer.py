36690c3e274966e6c81856d57df81dd323e7dcee
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-11 17:38:42 +0200

    Finish get_mac function

--- Ethicalhackwiz/arp_spoofer.py
+++ Ethicalhackwiz/arp_spoofer.py
@@ -6,3 +6,5 @@ def get_mac(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc
