0ad5e90e8f2efaf3bb670244b5c78ee6910bbfb6
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-11 17:46:38 +0200

    Add Print packet sending

--- Ethicalhackwiz/arp_spoofer.py
+++ Ethicalhackwiz/arp_spoofer.py
@@ -20,3 +20,5 @@ sent_packets_count = 0
 while True:
     spoof("xx.xx.xx.xx", "xx.xx.xx.xx") #spoofs IP to pose as the router
     spoof("xx.xx.xx.xx", "xx.xx.xx.xx")
+    sent_packets_count = sent_packets_count + 2
+    print(f"\r[+] Packets sent: {sent_packets_count}", end="")
