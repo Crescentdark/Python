e81f7ccdcd7de8086ebdc7f88859da0c4745804b
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-15 21:34:00 +0200

    Add more arguments

--- Ethicalhackwiz/replace_downloads.py
+++ Ethicalhackwiz/replace_downloads.py
@@ -14,3 +14,5 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in str(scapy_packet[scapy.raw].load):
+            
