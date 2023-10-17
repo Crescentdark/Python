34c8c126785bdd4d68be43a1d7deb02e3cc8564f
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-17 22:11:38 +0200

    Add arguments

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -7,7 +7,7 @@ class Scoreboard(Turtle):
     
     def __init__(self):
         super().__init__()
-        
+        self.level = 1
         self.hideturtle()
         self.penup()
         self.write(f"Level: ")
