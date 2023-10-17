437690dabd2b3873299d7b479add0501c4f58c61
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-17 22:26:05 +0200

    add game over

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -30,6 +30,7 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     #detect successful cross
     if player.is_at_finish_line():
