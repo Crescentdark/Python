401429494f3bc7eba32596fb0580080b78be5c84
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-18 12:35:22 +0200

    Move

---
+++ Excerssises/tip_calculator.py
@@ -0,0 +1,15 @@
+print("Welcome to the tip Calculator")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+split = int(input("How many people to split bill? "))
+
+tip_percent = tip / 100
+total_tip_amount = bill * tip_percent
+total_bill = bill + total_tip_amount
+
+result = total_bill / split
+
+round(result, 2)
+final = "{:.2f}".format(result)
+
+print(f"Each person should pay:  ${final}")
