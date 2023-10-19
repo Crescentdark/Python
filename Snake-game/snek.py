c3387cf604f8f3e1874009d1f612632939cfbeca
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-19 09:04:35 +0200

    Add more arguments

--- Snake-game/snek.py
+++ Snake-game/snek.py
@@ -38,14 +38,14 @@ while game_is_on:
 
 #Detect collision
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
-
+        #game_is_on = False
+        scoreboard.reset()
 #Detect collison with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            #game_is_on = False
+            scoreboard.reset()
+            snake.reset()
 
 
 
