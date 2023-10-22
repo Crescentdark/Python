a9111cee2947280009d3891b58f204d7b325ca2e
Crescentdark <114867953+Crescentdark@users.noreply.github.com>
2023-10-22 12:34:48 +0200

    Add more arguments

--- stockchecker/main.py
+++ stockchecker/main.py
@@ -5,7 +5,15 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from email.message import EmailMessage
 
+
+
 def main():
+    avaliable = check.availability()
+
+
+
+
+
 
     if __name__ == '__name__':
-        
+        main()
